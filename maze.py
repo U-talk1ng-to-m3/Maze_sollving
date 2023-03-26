999533b5e0a1e2902dfe8929a9dc6b967cbaf809
U-talk1ng-to-m3 <73353676+U-talk1ng-to-m3@users.noreply.github.com>
2023-03-26 19:07:32 +0300

    Update maze.py

--- maze.py
+++ maze.py
@@ -8,7 +8,7 @@ from collections import deque
 grid=[]
 satir=0
 with open ('url2.txt','r') as file:
-   for line in file.read().splitlines():
+    for line in file.read().splitlines():
      
      grid.append(line)
      satir=satir+1
@@ -33,23 +33,23 @@ print(grid)
 
 
 
-wn = turtle.Screen()               # define the turtle screen
-wn.bgcolor("black")                # set the background colour
+wn = turtle.Screen()               
+wn.bgcolor("black")                
 wn.title("Prolab II proje I")
-wn.setup(1300,700)                  # setup the dimensions of the working window
+wn.setup(1300,700)                  
 
 
     
-# this is the class for the Maze
-class Maze(turtle.Turtle):               # define a Maze class
+
+class Maze(turtle.Turtle):               
     def __init__(self):
         turtle.Turtle.__init__(self)
-        self.shape("square")            # the turtle shape
-        self.color("white")             # colour of the turtle
-        self.penup()                    # lift up the pen so it do not leave a trail
+        self.shape("square")           
+        self.color("white")             
+        self.penup()                    
         self.speed(0)
 
-# this is the class for the finish line - green square in the maze
+
 class Green(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
@@ -67,7 +67,7 @@ class Blue(turtle.Turtle):
         self.speed(0)
 
 
-# this is the class for the yellow or turtle
+
 class Red(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
@@ -88,31 +88,29 @@ class Yellow(turtle.Turtle):
 
 
 
-def setup_maze(grid):                          # define a function called setup_maze
-    global start_x, start_y, end_x, end_y      # set up global variables for start and end locations
-    for y in range(len(grid)):                 # read in the grid line by line
-        for x in range(len(grid[y])):          # read each cell in the line
-            character = grid[y][x]             # assign the varaible "character" the the x and y location od the grid
-            screen_x = -288 + (x * 24)         # move to the x location on the screen staring at -588
-            screen_y = 288 - (y * 24)          # move to the y location of the screen starting at 288
+def setup_maze(grid):                         
+    global start_x, start_y, end_x, end_y      
+    for y in range(len(grid)):                 
+        for x in range(len(grid[y])):          
+            character = grid[y][x]             
+            screen_x = -588 + (x * 24)         
+            screen_y = 288 - (y * 24)          
 
             if character == "+":
-                maze.goto(screen_x, screen_y)         # move pen to the x and y locaion and
-                maze.stamp()                          # stamp a copy of the turtle on the screen
-                walls.append((screen_x, screen_y))    # add coordinate to walls list
-
+                maze.goto(screen_x, screen_y)         
+                maze.stamp()                          
+                walls.append((screen_x, screen_y))    
             if character == " " or character == "e":
-                path.append((screen_x, screen_y))     # add " " and e to path list
-
+                path.append((screen_x, screen_y))     
             if character == "e":
                 green.color("purple")
-                green.goto(screen_x, screen_y)       # send green sprite to screen location
-                end_x, end_y = screen_x,screen_y     # assign end locations variables to end_x and end_y
+                green.goto(screen_x, screen_y)       
+                end_x, end_y = screen_x,screen_y     
                 green.stamp()
                 green.color("green")
 
             if character == "s":
-                start_x, start_y = screen_x, screen_y  # assign start locations variables to start_x and start_y
+                start_x, start_y = screen_x, screen_y  
                 red.goto(screen_x, screen_y)
 
 
@@ -124,19 +122,18 @@ def search(x,y):
     frontier.append((x, y))
     solution[x,y] = x,y
 
-    while len(frontier) > 0:          # exit while loop when frontier queue equals zero
+    while len(frontier) > 0:          
         time.sleep(0)
-        x, y = frontier.popleft()     # pop next entry in the frontier queue an assign to x and y location
+        x, y = frontier.popleft()     
 
-        if(x - 24, y) in path and (x - 24, y) not in visited:  # check the cell on the left
+        if(x - 24, y) in path and (x - 24, y) not in visited:  
             cell = (x - 24, y)
-            solution[cell] = x, y    # backtracking routine [cell] is the previous cell. x, y is the current cell
-            blue.goto(cell)        # identify frontier cells
+            solution[cell] = x, y    
+            blue.goto(cell)        
             blue.stamp()
-            frontier.append(cell)   # add cell to frontier list
-            visited.add((x-24, y))  # add cell to visited list
-
-        if (x, y - 24) in path and (x, y - 24) not in visited:  # check the cell down
+            frontier.append(cell)   
+            visited.add((x-24, y))  
+        if (x, y - 24) in path and (x, y - 24) not in visited:  
             cell = (x, y - 24)
             solution[cell] = x, y
             blue.goto(cell)
@@ -145,7 +142,7 @@ def search(x,y):
             visited.add((x, y - 24))
             print(solution)
 
-        if(x + 24, y) in path and (x + 24, y) not in visited:   # check the cell on the  right
+        if(x + 24, y) in path and (x + 24, y) not in visited:   
             cell = (x + 24, y)
             solution[cell] = x, y
             blue.goto(cell)
@@ -153,41 +150,42 @@ def search(x,y):
             frontier.append(cell)
             visited.add((x +24, y))
 
-        if(x, y + 24) in path and (x, y + 24) not in visited:  # check the cell up
+        if(x, y + 24) in path and (x, y + 24) not in visited:  
             cell = (x, y + 24)
             solution[cell] = x, y
-            #blue.goto(cell)
-            #blue.stamp()
+            
+            
             frontier.append(cell)
             visited.add((x, y + 24))
         green.goto(x,y)
         green.stamp()
+        
 
 
 def backRoute(x, y):
     yellow.goto(x, y)
     yellow.stamp()
-    while (x, y) != (start_x, start_y):    # stop loop when current cells == start cell
-        yellow.goto(solution[x, y])        # move the yellow sprite to the key value of solution ()
+    while (x, y) != (start_x, start_y):    
+        yellow.goto(solution[x, y])        
         yellow.stamp()
-        x, y = solution[x, y]               # "key value" now becomes the new key
+        x, y = solution[x, y]               
 
-# set up classes
+
 maze = Maze()
 red = Red()
 blue = Blue()
 green = Green()
 yellow = Yellow()
 
-# setup lists
+
 walls = []
 path = []
 visited = set()
 frontier = deque()
-solution = {}                           # solution dictionary
+solution = {}                           
 
 
-# main program starts here ####
+
 setup_maze(grid)
 search(start_x,start_y)
 backRoute(end_x, end_y)
